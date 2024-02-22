805a741438ef4d053e1edfd9c8b1243a90fb6c6d
JessCP25 <jpayanoc25@gmail.com>
2024-02-21 22:15:57 -0500

    Uso de inputs y outputs

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input({required: true}) image: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
 
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler(){
+    this.addToCart.emit('Hola soy un mensaje del hijo');
+  }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  fromChild(value: string){
+    console.log('Soy el padre', value)
+  }
 }

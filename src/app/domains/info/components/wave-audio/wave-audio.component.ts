057ece038517ee1857726a298187007c1598ba61
JessCP25 <jpayanoc25@gmail.com>
2024-02-25 22:03:36 -0500

    ngAfterViewInit - ciclo de vida

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-wave-audio',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './wave-audio.component.html',
   styleUrl: './wave-audio.component.css'
 })
-export class WaveAudioComponent {
+export class WaveAudioComponent implements AfterViewInit {
+  @Input({required: true}) audioUrl!: string;
+  @ViewChild('wave') container !: ElementRef;
+  private ws!: WaveSurfer;
+  isPlay = signal(false);
 
+  ngAfterViewInit(){
+    this.ws = WaveSurfer.create({
+      url: this.audioUrl,
+      container: this.container.nativeElement
+    })
+    this.ws.on('play', () => this.isPlay.set(true));
+    this.ws.on('pause', () => this.isPlay.set(false));
+  }
+
+  playPause(){
+    this.ws.playPause()
+  }
 }
--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,24 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
+import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,CounterComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
-  changeDuration(event: Event){
+  changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
   }
 
-  changeMessage(event: Event){
+  changeMessage(event: Event) {
     const input = event.target as HTMLInputElement;
     this.message.set(input.value);
   }

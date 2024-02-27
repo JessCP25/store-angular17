ebc9fbf141f139b4c837a52b2eaaf9e10f2a6f01
JessCP25 <jpayanoc25@gmail.com>
2024-02-26 22:53:41 -0500

    creando header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,19 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
-
   products = signal<Product[]>([]);
 
-  constructor(){
+  constructor() {
     const initProducts: Product[] = [
       {
         id: Date.now(),
@@ -36,11 +36,74 @@ export class ListComponent {
         price: 150,
         image: 'https://picsum.photos/640/640?r=10',
         creationAt: new Date().toISOString(),
-      }
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=20',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=13',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=11',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=31',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=44',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=18',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=21',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=17',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=50',
+        creationAt: new Date().toISOString(),
+      },
     ];
     this.products.set(initProducts);
   }
-  fromChild(value: string){
-    console.log('Soy el padre', value)
+  fromChild(value: string) {
+    console.log('Soy el padre', value);
   }
 }

ee4f84ebc93ea883d698d02db281903d95c697eb
JessCP25 <jpayanoc25@gmail.com>
2024-02-26 22:24:34 -0500

    Creando componente de productos

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -8,9 +9,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  @Input({required: true}) image: string = '';
-  @Input({required: true}) price: number = 0;
-  @Input({required: true}) title: string = '';
+  @Input({required: true}) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,35 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=14',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=10',
+        creationAt: new Date().toISOString(),
+      }
+    ];
+    this.products.set(initProducts);
+  }
   fromChild(value: string){
     console.log('Soy el padre', value)
   }

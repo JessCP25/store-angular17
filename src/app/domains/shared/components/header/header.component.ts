5268ce267f15735280e9eb539b6418b9e3e33db2
JessCP25 <jpayanoc25@gmail.com>
2024-02-29 23:28:46 -0500

    Calculando el total del cart con signal

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
@@ -8,11 +8,23 @@ import { Product } from '../../models/product.model';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges{
   hideSideMenu = signal(true);
   @Input({required: true}) cart: Product[] = [];
+  total = signal(0);
 
   toggleSideMenu() {
     this.hideSideMenu.update((prevState) => !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const cart = changes['cart'];
+    if(cart){
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product)=> total + product.price, 0)
   }
 }
